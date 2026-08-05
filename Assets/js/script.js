12fe40c1c0c32783a8d2d9f6f0cbb5d639f6100d
devFkauan <kaungc132@gmail.com>
2026-08-05 16:41:58 -0300

    Primeiros passos

---
+++ Assets/js/script.js
@@ -0,0 +1,40 @@
+function addTimer () {
+    const relogio = document.querySelector('.relogio')
+    function dateLocalTimerFromSeconds (seconds) {
+        let data = new Date( seconds * 1000)
+
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        })
+    }
+
+    let seconds = 0;
+    let timer;
+
+    function startTimer () {
+        timer = setInterval(() => {
+            seconds++
+            relogio.innerHTML = dateLocalTimerFromSeconds(seconds);
+        }, 1000)
+    }
+
+    document.addEventListener('click', (e) => {
+        const el = e.target;
+        
+        if (el.classList.contains('iniciar')) {
+            clearInterval(timer);
+            startTimer();
+        }
+        
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+        }
+
+        if (el.classList.contains('zerar')) {
+            relogio.innerHTML = '00:00:00';
+        }
+    })
+}
+
+addTimer()
